4bc82b70ec39896ee63995b14388e010336bb86e
Mad Marine <pnnroman92@gmail.com>
2020-03-03 16:04:32 -0600

    now a summary element appears when the user clicks on one of the autogenrated movies produced by our api

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ const onInput = async event => {
     option.addEventListener('click', () => {
       dropdown.classList.remove('is-active');
       input.value = movie.Title;
+      onMovieSelect(movie);
     })
     resultsWrapper.appendChild(option);
   }
@@ -59,4 +60,39 @@ document.addEventListener('click', event => {
   if (!root.contains(event.target)) {
     dropdown.classList.remove('is-active');
   }
-})
+});
+
+const onMovieSelect = async movie => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'f6434729',
+      i: movie.imdbID
+    }
+  });
+
+  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+
+}
+
+const movieTemplate = (movieDetail) => {
+  return `
+  <article class="media">
+  <figure class="media-left">
+  <p class="image">
+  <img src="${movieDetail.Poster}" />
+  </p>
+  </figure>
+  <div class="media-content">
+  <div class="content">
+  <h1> ${movieDetail.Title}</h1>
+  <h4>${movieDetail.Genre}</h4>
+  <p>${movieDetail.Plot}</p>
+  </div>
+  </div>
+  </article>
+  <article class="notification is-primary">
+  <p class="title">${movieDetail.Awards}</p>
+  <p class="subtitle">Awards</p>
+  </article>
+  `
+}
