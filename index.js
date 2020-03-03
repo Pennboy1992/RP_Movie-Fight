2063e1cf64fc6cfb2d5a25ef820024b25fb58182
Mad Marine <pnnroman92@gmail.com>
2020-03-03 02:19:09 -0600

    data is now being pulled on inputs in search box

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 // API Key
 // f6434729
 
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: 'f6434729',
-      s: 'avengers'
+      s: searchTerm
     }
   });
 
   console.log(response.data);
 };
 
-fetchData();
+
+const input = document.querySelector('input');
+
+input.addEventListener('input', (event) => {
+  fetchData(event.target.value);
+})
