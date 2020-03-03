15e63203d16f2bfeafec7dc110e4de665cfddb0e
Mad Marine <pnnroman92@gmail.com>
2020-03-02 23:39:11 -0600

    successfully pulling data from omdbapi

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hi there!')
+// API Key
+// f6434729
+
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'f6434729',
+      s: 'avengers'
+    }
+  });
+
+  console.log(response.data);
+};
+
+fetchData();
