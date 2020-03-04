7d4e3b7ca4455f9cc3ce3615d5b02cf2568a56ee
Mad Marine <pnnroman92@gmail.com>
2020-03-03 23:10:13 -0600

    I'm successfully pulling the numbers out of the strings of the api

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const autocompleteConfig = {
     ${movie.Title} (${movie.Year})
     `;
   },
-  onOptionSelect(movie) {
-    onMovieSelect(movie)
-  },
   inputValue(movie) {
     return movie.Title;
   },
@@ -31,12 +28,23 @@ const autocompleteConfig = {
 createAutoComplete({
   ...autocompleteConfig,
   root: document.querySelector('#left-autocomplete'),
+  onOptionSelect(movie) {
+    document.querySelector('.tutorial').classList.add('is-hidden');
+    onMovieSelect(movie, document.querySelector('#left-summary'), 'left');
+  }
 });
 createAutoComplete({
   ...autocompleteConfig,
   root: document.querySelector('#right-autocomplete'),
+  onOptionSelect(movie) {
+    document.querySelector('.tutorial').classList.add('is-hidden');
+    onMovieSelect(movie, document.querySelector('#right-summary'), 'right');
+  }
 });
-const onMovieSelect = async movie => {
+
+let leftMovie;
+let rightMovie;
+const onMovieSelect = async (movie, summaryElement, side) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: 'f6434729',
@@ -44,11 +52,34 @@ const onMovieSelect = async movie => {
     }
   });
 
-  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+  summaryElement.innerHTML = movieTemplate(response.data);
 
+  if (side === 'left') {
+    leftMovie = response.data;
+  } else {
+    rightMovie = response.data;
+  }
+
+  if (leftMovie && rightMovie) {
+    runComparison();
+  }
+
+};
+
+const runComparison = () => {
+  console.log('Time for comparison');
 }
 
 const movieTemplate = (movieDetail) => {
+
+  const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, ''));
+  const metascore = parseInt(movieDetail.Metascore);
+  const imdbRating = parseFloat(movieDetail.imdbRating);
+  const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''));
+
+
+
+
   return `
   <article class="media">
   <figure class="media-left">
