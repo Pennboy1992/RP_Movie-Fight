0ed876181efc1638951ada1c6e1f9e59121f48de
Mad Marine <pnnroman92@gmail.com>
2020-03-03 22:03:30 -0600

    completed my reusablity refactor so now I can reuse my autocomplete component for other projects

--- autocomplete.js
+++ autocomplete.js
@@ -15,22 +15,22 @@ const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue, fe
   const onInput = async event => {
     const items = await fetchData(event.target.value);
 
-    if (!movies.length) {
+    if (!items.length) {
       dropdown.classList.remove('is-active');
       return;
     }
     resultsWrapper.innerHTML = '';
     dropdown.classList.add('is-active');
-    for (let movie of movies) {
+    for (let item of items) {
       const option = document.createElement('a');
 
 
       option.classList.add('dropdown-item');
-      option.innerHTML = renderOption(movie);
+      option.innerHTML = renderOption(item);
       option.addEventListener('click', () => {
         dropdown.classList.remove('is-active');
-        input.value = inputValue(movie);
-        onOptionSelect(movie);
+        input.value = inputValue(item);
+        onOptionSelect(item);
       })
       resultsWrapper.appendChild(option);
     }
